d8fb7e93091213c7e6af96f1d38a161ed0cf1c6b
Eddard <dsuborno0@gmail.com>
2026-05-29 17:06:41 +0530

    Updated Readme FIle

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-import '../css/app.css'
+// import '../css/app.css'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
